fd6eee23ab278908b4560c8ad532d227266f86a1
Elliot Marsden <elliot.marsden@gmail.com>
2014-03-12 13:41:19 +0000

    Dead

---
+++ content/index.md
@@ -0,0 +1,9 @@
+Title: Our Friend Fibre
+Date: 2014-03-12 13:01
+Category: Food
+Tags: fibre
+Slug: fibre
+Author: Elliot Marsden
+Summary: The unbreakable bond between man and fibre
+
+Fibre is all around us, and so it should be.
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Elliot Marsden'
+SITENAME = u'Small Bangs'
+SITEURL = ''
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+           )
+
+# Social widget
+SOCIAL = (('Facebook', 'https://www.facebook.com/elmo.marsden'),
+          ('Twitter', 'https://twitter.com/EddieJessup'),
+          ('Work', 'http://ph.ed.ac.uk/people/elliot-marsden'),
+          ('GitHub', 'https://github.com/eddiejessup'),
+          )
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
